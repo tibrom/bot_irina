d9fd54d48169beb002ce5caa69860b005fb21b3b
Andrey <gorlans@mail.ru>
2023-11-06 12:52:10 +0300

    acho

--- server/endpoints.py
+++ server/endpoints.py
@@ -1,9 +1,10 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Request
 
 from .depends import get_main_reposytory
 from .models import MessageType
 from .repositories import MainRepository
+from core.logger import logger
 
 
 
@@ -18,3 +19,25 @@ async def send_new_message(
     message_type: MainRepository =Depends(get_main_reposytory)
 ):
     return await message_type.send_message(message=new)
+
+
+@router.post('/echo')
+async def echo_request(request: Request):
+    request_body = "None"
+    request_headers = "None"
+    request_data = "None"
+    try:
+        request_body = await request.body()
+    except:
+        pass
+    try:
+        request_headers = dict(request.headers)
+    except:
+        pass
+    try:
+        request_data = await request.json()
+    except:
+        pass
+    logger.debug(f"request_body {request_body}")
+    logger.debug(f"request_headers {request_headers}")
+    logger.debug(f"request_data {request_data}")
